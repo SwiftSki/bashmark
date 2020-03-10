16f768f29af9fc2bba87557737099b924bc8c504
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-03-10 10:42:16 -0600

    added code that will in the future add accounts
    json file stuff needs to be set up

--- create.js
+++ create.js
@@ -1,3 +1,16 @@
 //add stuff to add accounts to the json
 //notify when new accounts are created
 //check for duplicate accounts
+
+function createId(){
+  var id = prompt('what would you like you id to be?');
+  //add something to get account stuff
+  if(id === /* existing account ids*/){
+    alert('that id has been taken');
+    createId();
+  }
+  else{
+    var success=prompt('ctrl + c\nthen add a new bookmark and replace the url with that code','javascript: var n='+id+'; var x=document.createElement("script"); x.src="repl url here"; document.body.appendChild(x);');
+  }
+}
+createId();
