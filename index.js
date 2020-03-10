b0daaad20e87c49a489276fcdef326c34a9ea92b
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-03-10 10:45:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 //probably call the JSON for accounts
 //then some logic for who can do what
 //and security and tracking
+//jsdelivr can be used here, but a repl needs to be created for dynamic updates
