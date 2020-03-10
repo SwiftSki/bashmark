9e492caa1191578056c95a0911891c0f70b5b2a6
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-03-10 12:27:09 -0600

    Update create.js

--- create.js
+++ create.js
@@ -2,15 +2,14 @@
 //notify when new accounts are created
 //check for duplicate accounts
 
+var accountinfo = document.createElement('script');
+accountinfo.src='';
+document.body.appendChild(accountinfo);
+
+var data = accountinfo.parse(js);
+
 function createId(){
-  var id = prompt('what would you like you id to be?');
-  //add something to get account stuff
-  if(id === /* existing account ids*/){
-    alert('that id has been taken');
-    createId();
-  }
-  else{
-    var success=prompt('ctrl + c\nthen add a new bookmark and replace the url with that code','javascript: var n='+id+'; var x=document.createElement("script"); x.src="repl url here"; document.body.appendChild(x);');
-  }
+  var id = data.length + 1;
+  var success = prompt('copy the highlighted code\nthen add a new bookmark (right click on the bookmarks bar and click add page)\nand replace the url with that code','javascript: var n=' + id + '; var x=document.createElement("script"); x.src="repl url here"; document.body.appendChild(x);');
 }
 createId();
