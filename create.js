0cbabbe93da5f4d94355168a3049c1d6b655ed81
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-03-11 14:29:54 -0600

    Update create.js

--- create.js
+++ create.js
@@ -9,7 +9,18 @@ document.body.appendChild(accountinfo);
 var data = accountinfo.parse(js);
 
 function createId(){
+  var name = prompt('What is you full name?\n(first and last)','John Doe');
   var id = data.length + 1;
-  var success = prompt('copy the highlighted code\nthen add a new bookmark (right click on the bookmarks bar and click add page)\nand replace the url with that code','javascript: var n=' + id + '; var x=document.createElement("script"); x.src="repl url here"; document.body.appendChild(x);');
+  var code = 'javascript: var n=' + id + '; var x=document.createElement("script"); x.src="repl url here"; document.body.appendChild(x);';
+  var success = prompt('copy the highlighted code\nthen add a new bookmark (right click on the bookmarks bar and click add page)\nand replace the url with that code\npress cancel to cancel stuff on my end',code);
+  var pushname = '{"' + name + '"}';
+  ww
+  if(success === code){
+    data.accounts.push(pushname);
+    accountinfo = JSON.stringify(data);
+  }
+  else{
+    alert('please contact me for help');
+  }
 }
 createId();
